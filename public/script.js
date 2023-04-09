71c206c5bd320765a82c3c6d13108bee08b518a3
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-04-09 12:49:16 -1000

    update fetch url

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ $button.on('click', () => {
     var userInput = $("input").val()
 
 
-fetch(`http://127.0.0.1:3000/api/cars/${userInput}`)
+fetch(`https://newfrontend.onrender.com/api/cars/${userInput}`)
   .then(response => response.json())
   .then(data => {
 
