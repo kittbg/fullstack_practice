21810d78b98a00a2712cdda81c78f10382ff501f
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-04-09 13:52:54 -1000

    clean slate

--- migration.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS cars CASCADE;
-
-CREATE TABLE cars(
-    id serial PRIMARY KEY,
-    make varchar(30),
-    model varchar(30),
-    year integer,
-    color varchar(30)
-);
--- seed.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO cars (make, model, year, color) VALUES ('Ford', 'F-150', 2023, 'Red');
-INSERT INTO cars (make, model, year, color) VALUES ('Tesla', 'Model Y', 2020, 'Gray');
-INSERT INTO cars (make, model, year, color) VALUES ('Jeep', 'Wrangler', 2021, 'Blue');
-INSERT INTO cars (make, model, year, color) VALUES ('Ford', 'Mustang', 2019, 'Black');
-INSERT INTO cars (make, model, year, color) VALUES ('Jeep', 'Galdiator', 2022, 'Yellow');
-INSERT INTO cars (make, model, year, color) VALUES ('Tesla', 'Model S', 2023, 'Bright Red');
