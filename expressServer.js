5474011570472984f02dca1f02d6deebb2cb1445
Brad Kittinger <123213712+kittbg@users.noreply.github.com>
2023-04-09 17:26:46 -1000

    updated with all routes

--- expressServer.js
+++ expressServer.js
@@ -29,6 +29,63 @@ app.get('/api/cars/:make', async (req, res)=>{
    }
 })
 
+app.post('/api/cars', async (req, res)=>{
+    let make = req.body.make;
+    let model = req.body.model;
+    let year = req.body.year;
+    let color = req.body.color;
+    try{
+        await client.query('INSERT INTO cars (make, model, year, color) VALUES ($1, $2, $3, $4)', [make, model, year, color]);
+        res.send('New car entered!')
+    } catch (error){
+        console.error(error);
+        res.status('400').send('Addition failed.')
+    }
+})
+
+app.put('/api/cars/:id', async (req, res)=>{
+    let make = req.body.make;
+    let model = req.body.model;
+    let year = req.body.year;
+    let color = req.body.color;
+    try{
+        await client.query(`UPDATE cars SET make =$1, model = $2, year = $3, color = $4 WHERE id = $5`, [make, model, year, color, req.params.id]);
+        res.send('New car entered!')
+    } catch (error){
+        console.error(error);
+        res.status('400').send('Addition failed.')
+    }
+})
+
+app.patch('/api/cars/:id', async (req, res)=> {
+    let id = req.params.id;
+    let key = Object.keys(req.body)[0];
+    let value = Object.values(req.body)[0];
+    try{
+        console.log(`Updating car with id ${id} and setting ${key} to ${value}`);
+     let results = await client.query(`UPDATE cars SET `+ key +` = $1 WHERE id = $2`, [value, id]);
+     if (results.rowCount === 0){
+        res.status(404).send(`Car with ${id} was not found.`)
+     } else {
+        res.send(results.rows)
+     }
+    } catch (error){
+        console.error(error);
+        res.status(400).send('Patch req failed.');
+    }
+})
+
+app.delete('/api/cars/:id', async (req, res) =>{
+    const id = req.params.id;
+    try {
+    await client.query('DELETE FROM cars WHERE id = $1', [id]);
+    res.send(`Car with ${id} was deleted`);
+    } catch (error){
+        console.error(error);
+        res.status(404).send('Entity not deleted')
+    }
+})
+
 
 app.listen(port, (error)=>{
     if (error){
